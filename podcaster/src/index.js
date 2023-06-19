39ea1f4e491ea5a7bfa6c25bd12a17d370d3fb5c
Francisco Ximenes <francisco.ximenes@fabamaq.com>
2023-06-19 20:36:49 +0200

    [FRONTEND] - add index.js

--- podcaster/src/index.js
+++ podcaster/src/index.js
@@ -1,37 +1,11 @@
 import * as React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import Wrapper from "components/utils/Wrapper";
-import ListProvider from "contexts/ListContext";
-import PodcastDetail from "components/views/podcastDetail/PodcastDetail";
-import PodcastAudio from "components/views/podcastAudio/PodcastAudio";
-import PodcastList from "components/views/podcastList/PodcastList";
+import App from "./App";
 
-import "styles/app.scss";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ListProvider>
-        <PodcastList />
-      </ListProvider>
-    ),
-  },
-  {
-    path: "podcast/:podcastId",
-    element: <PodcastDetail />,
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
-  <Wrapper>
-    <RouterProvider router={router} />
-  </Wrapper>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
